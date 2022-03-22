e2c436a293fb0ad76f57a6fbe75e8405b24fe55e
maestroware <83496883+maestroware@users.noreply.github.com>
2022-03-22 15:03:44 +0530

    Update log.js

--- log.js
+++ log.js
@@ -7,6 +7,11 @@ function signIn() {
         alert("Signed in as Arjun Maheshwari")
         window.location = "ceo.html"
     }
+    if (madd == "manish.maheshwari@maestroware.ind" && mpass == "Manish1!!") {
+        auser = "Manish Maheshwari"
+        alert("Signed in as Manish Maheshwari")
+        window.location = "index.html"
+    }
 }
 
 function logout1() {
